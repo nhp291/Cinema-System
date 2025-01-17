da2a5b15644ddbce5950785e59f5b3d8acad25da
nhp291 <nhp2901@gmail.com>
2025-01-17 08:14:08 +0700

    code FE

---
+++ Cinema_System/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieDetailPage: React.FC = () => {
+    return (
+        <div className='movie-detail-page'>
+            <h1>Movie Detail Page</h1>
+            <p>This is the movie detail page</p>
+        </div>
+    );
+};
+
+export default MovieDetailPage;
